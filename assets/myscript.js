c184483c9ae48c81c1a5bd958b6429af7d09a267
MrigankaIsHere <55996902+MrigankaIsHere@users.noreply.github.com>
2021-06-23 10:31:02 +0530

    Non-IIT student updates

--- assets/myscript.js
+++ assets/myscript.js
@@ -21,7 +21,7 @@
           typeWriter();
       }
       var i = 0;
-      var txt = 'Hi there. My name is Mriganka Shekhar Chakravarty. Quite a mouthfull! Since you have arrived at my page, I assume you took at the least, the minimal interest in me. Here is a brief intro to my non-academic history: /n/n I was born on the 10th of October in the year 1996./nMy home is situated in Kolkata. Currently I am living in Kanpur. I am good at playing Cricket and Table Tennis. If you ever meet me somewhere near a table tennis board, trust me, we will have an awesome time./nI am a big admirer of stand up Comedy. Russel Peters and Louis CK are my kind of comedians. Daniel Fernandes and Zakir Khan are my favorite indigenous comedians.'; /* The text */
+      var txt = 'Hi there. My name is Mriganka Shekhar Chakravarty. Quite a mouthfull! Since you have arrived at my page, I assume you took at the least, the minimal interest in me. Here is a brief intro to my non-academic history: <br><br> I was born on the 10th of October in the year 1996.<br>My home is situated in Kolkata. These days, I am virually living all over the world. I am good at playing Cricket and Table Tennis. If you ever meet me somewhere near a table tennis board, trust me, we will have an awesome time.<br>I am a big admirer of stand up Comedy. Russel Peters and Louis CK are my kind of comedians. Daniel Fernandes and Zakir Khan are my favorite indigenous comedians.'; /* The text */
       var speed = 5; /* The speed/duration of the effect in milliseconds */
       function typeWriter() {
         if (screen.width> 700 && i < txt.length) {
@@ -36,7 +36,7 @@
           setTimeout(typeWriter, speed);
         }
         else if(screen.width<=700) {
-	  txt= 'Hi there. My name is Mriganka Shekhar Chakravarty. Quite a mouthfull! Since you have arrived at my page, I assume you took at the least, the minimal interest in me. Here is a brief intro to my non-academic history: <br><br> I was born on the 10th of October in the year 1996.<br>My home is situated in Kolkata. Currently I am living in Kanpur. I am good at playing Cricket and Table Tennis. If you ever meet me somewhere near a table tennis board, trust me, we will have an awesome time.<br>I am a big admirer of stand up Comedy. Russel Peters and Louis CK are my kind of comedians. Daniel Fernandes and Zakir Khan are my favorite indigenous comedians.'; /* The text */
+	  txt= 'Hi there. My name is Mriganka Shekhar Chakravarty. Quite a mouthfull! Since you have arrived at my page, I assume you took at the least, the minimal interest in me. Here is a brief intro to my non-academic history: <br><br> I was born on the 10th of October in the year 1996.<br>My home is situated in Kolkata. These days, I am virually living all over the world. I am good at playing Cricket and Table Tennis. If you ever meet me somewhere near a table tennis board, trust me, we will have an awesome time.<br>I am a big admirer of stand up Comedy. Russel Peters and Louis CK are my kind of comedians. Daniel Fernandes and Zakir Khan are my favorite indigenous comedians.'; /* The text */
 
           document.getElementById("printer").innerHTML += txt;
         }
